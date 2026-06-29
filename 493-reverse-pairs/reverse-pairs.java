b88c50034f77b8088e672e08d57e8493ad6eb9c9
devanubhav01 <anubhavsingh18599@gmail.com>
2026-06-29 20:50:31 +0530

    Time: 43 ms (75.96%) | Memory: 64.2 MB (49.23%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.java
@@ -0,0 +1,83 @@
+class Solution {
+    public int reversePairs(int[] nums) {
+         return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort(int[] arr, int low, int high) {
+
+        if (low >= high) {
+            return 0;
+        }
+
+        int count = 0;
+
+        int mid = low + (high - low) / 2;
+
+        count += mergeSort(arr, low, mid);
+
+        count += mergeSort(arr, mid + 1, high);
+
+        count += countPairs(arr, low, mid, high);
+
+        merge(arr, low, mid, high);
+
+        return count;
+    }
+
+    private int countPairs(int[] arr, int low, int mid, int high) {
+
+        int count = 0;
+
+        int right = mid + 1;
+
+        for (int left = low; left <= mid; left++) {
+
+            while (right <= high &&
+                   arr[left] > 2L * arr[right]) {
+
+                right++;
+            }
+
+            count += (right - (mid + 1));
+        }
+
+        return count;
+    }
+
+    private void merge(int[] arr, int low, int mid, int high) {
+
+        int[] temp = new int[high - low + 1];
+
+        int left = low;
+        int right = mid + 1;
+        int k = 0;
+
+        while (left <= mid && right <= high) {
+
+            if (arr[left] <= arr[right]) {
+
+                temp[k++] = arr[left++];
+
+            } else {
+
+                temp[k++] = arr[right++];
+            }
+        }
+
+        while (left <= mid) {
+
+            temp[k++] = arr[left++];
+        }
+
+        while (right <= high) {
+
+            temp[k++] = arr[right++];
+        }
+
+        for (int i = 0; i < temp.length; i++) {
+
+            arr[low + i] = temp[i];
+        }
+
+    }
+}
