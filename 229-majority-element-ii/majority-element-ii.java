e76e5688a0a804cb45b21cb9e74776d7f4d08d08
ANUBHAV SINGH <anubhavsingh18599@gmail.com>
2026-07-31 20:12:17 +0530

    Time: 19 ms (11.59%) | Memory: 52.1 MB (83.57%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,43 +1,17 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1=0;
-        int candidate2=0;
-        int count1=0;
-        int count2=0;
-        for(int num : nums){
-            if(num==candidate1){
-                count1++;
-            }
-            else if(num==candidate2){
-                count2++;
-            }
-            else if(count1==0){
-                candidate1=num;
-                count1=1;
-            }
-            else if(count2==0){
-                candidate2=num;
-                count2=1;
-            }
-            else{
-                count1--;
-                count2--;
-            }
-        }
-        count1=0;
-        count2=0;
-        for(int num :nums){
-            if(num==candidate1)
-            count1++;
-            else if(num==candidate2)
-            count2++;
-        }
-        List<Integer> ans=new ArrayList<>();
-        int limit=nums.length/3;
-        if(count1>limit)
-        ans.add(candidate1);
-        if(count2>limit)
-        ans.add(candidate2);
-        return ans;
+        ArrayList<Integer> a=new ArrayList<>();
+        HashMap<Integer,Integer> arr=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+          arr.put(nums[i], arr.getOrDefault(nums[i], 0) + 1);
+          if (arr.get(nums[i]) > nums.length / 3&&!a.contains(nums[i])){
+            
+         a.add(nums[i]);
+            
+     }
+     }
+     
+     Collections.sort(a); 
+     return a;
     }
 }
